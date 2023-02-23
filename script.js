b0b41864c643b8d591855572bc2677c290fabe68
gerlecztamas <gerlecztamas@gmail.com>
2023-02-23 21:23:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,131 @@
+function getComputerChoice(){
+    let computerNumber = Math.floor(Math.random() * (3-1+1) + 1);
+    if (computerNumber === 1){
+        return "rock";
+    }
+    else if (computerNumber === 2){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
+}
+
+
+let reset = document.querySelector("#reset");
+reset.style.display = "none";
+
+let result = document.createElement("div");
+document.body.appendChild(result);
+
+
+let endresult = document.createElement("h1");
+document.body.appendChild(endresult);
+document.body.insertBefore(endresult, reset);
+document.body.insertBefore(result, endresult);
+
+
+
+let pscore = 0;
+let cscore = 0;
+
+let buttons = document.querySelectorAll(".choices");
+buttons.forEach(function(item){
+    item.addEventListener("click", event => {
+        reset.style.display = "block";
+        let playersChoice = playerSelection(item.textContent);
+        
+        result.textContent = roundOfRps(playersChoice, getComputerChoice());
+        
+    })
+});
+
+
+function playerSelection(playersChoice){
+    /*let playersChoice = prompt("Enter your choice!");*/
+    playersChoice = playersChoice.toLowerCase();
+    if (playersChoice === "rock" || playersChoice === "paper" || playersChoice == "scissors"){
+        return playersChoice;
+    }
+    else{
+        alert("Hey!");
+        return playerSelection();
+    }
+}
+
+function won(){
+    endresult.textContent = "The battle is over! We have won!";
+    reset.style.display = "block";
+}
+function lost(){
+    endresult.textContent = "The battle is over! We have lost!";
+    reset.style.display = "block";
+}
+
+function roundOfRps(playerSelection, computerChoice){
+    if ((playerSelection === "rock" && computerChoice === "scissors") || (playerSelection === "paper" && computerChoice === "rock") || (playerSelection === "scissors" && computerChoice === "paper")){
+        
+        pscore++;
+        if (pscore === 5){
+            won();
+        }
+        console.log(pscore, cscore);
+        return "You win! " + playerSelection + " beats " + computerChoice;
+    }
+    else if ((computerChoice === "rock" && playerSelection === "scissors") || (computerChoice === "paper" && playerSelection === "rock") || (computerChoice === "scissors" && playerSelection === "paper")){
+        
+        cscore++;
+        if(cscore === 5){
+            lost();
+        }
+        console.log(pscore, cscore);
+        return "You lose! " + computerChoice + " beats " + playerSelection;
+    }
+    else{
+        console.log(playerSelection, computerChoice);
+        return "Tie game!";
+    }
+}
+
+reset.addEventListener("click", event => {
+    pscore = 0;
+    cscore = 0;
+    endresult.textContent = "";
+    result.textContent = "";
+    reset.style.display = "none";
+});
+
+/*
+function game(){
+    let pwins = 0;
+    let cwins = 0;
+    let i = 0;
+    while (i < 5){
+        if (pwins === 3){
+            return "You won " + pwins + " to " + cwins;
+        }
+        else if(cwins === 3){
+            return "You lost " + pwins + " to " + cwins;
+        }
+        let result = roundOfRps(playerSelection(), getComputerChoice());
+        console.log(result);
+        if (result.substring(0,7) === "You win"){
+            pwins++;
+            i++;
+        }
+        else if (result.substring(0,8) === "You lose"){
+            cwins++;
+            i++;
+        }
+    }
+    if (pwins > cwins){
+        return "You won " + pwins + " to " + cwins;
+    }
+    else if (cwins > pwins){
+        return "You lost " + pwins + " to " + cwins;
+    }
+    else{
+        return "It was a tie";
+    }
+}
+*/
