5284361f14c043fce2f529365db3f9631c2640e1
gerlecztamas <gerlecztamas@gmail.com>
2023-02-24 23:26:02 +0100

    implementing the whole game

--- res/script.js
+++ res/script.js
@@ -1,46 +1,33 @@
-/*
-function getComputerChoice(){
-    let computerNumber = Math.floor(Math.random() * (3-1+1) + 1);
-    if (computerNumber === 1){
-        return "rock";
-    }
-    else if (computerNumber === 2){
-        return "paper";
-    }
-    else{
-        return "scissors";
-    }
-}
-
-
-let reset = document.querySelector("#reset");
-reset.style.display = "none";
-
-let result = document.createElement("div");
-document.body.appendChild(result);
-
-
-let endresult = document.createElement("h1");
-document.body.appendChild(endresult);
-document.body.insertBefore(endresult, reset);
-document.body.insertBefore(result, endresult);
-
-
-
 let pscore = 0;
 let cscore = 0;
 
-let buttons = document.querySelectorAll(".choices");
-buttons.forEach(function(item){
-    item.addEventListener("click", event => {
-        reset.style.display = "block";
-        let playersChoice = playerSelection(item.textContent);
-        
-        result.textContent = roundOfRps(playersChoice, getComputerChoice());
-        
-    })
+let game = document.querySelector(".game");
+game.style.display = "none";
+
+let play = document.getElementById("play");
+
+let begin = document.querySelector(".begin");
+
+let result = document.querySelector(".result");
+
+let standings = document.querySelector(".standings");
+
+let endresult = document.querySelector(".endresult");
+
+let reset = document.querySelector(".reset");
+
+play.addEventListener("click", event => {
+    begin.style.display = "none";
+    game.style.display = "block";
 });
 
+let choices = document.querySelectorAll(".choice");
+choices.forEach(function(item){
+    item.addEventListener("click", event => {
+        let playersChoice = playerSelection(item.textContent);
+        result.textContent = roundOfRps(playersChoice, getComputerChoice());
+    })
+});
 
 function playerSelection(playersChoice){
     
@@ -54,22 +41,21 @@ function playerSelection(playersChoice){
     }
 }
 
-function won(){
-    endresult.textContent = "The battle is over! We have won!";
-    reset.style.display = "block";
-    buttons.forEach(function(item){
-        item.disabled = true;
-    });
-}
-function lost(){
-    endresult.textContent = "The battle is over! We have lost!";
-    reset.style.display = "block";
-    buttons.forEach(function(item){
-        item.disabled = true;
-    });
+function getComputerChoice(){
+    let computerNumber = Math.floor(Math.random() * (3-1+1) + 1);
+    if (computerNumber === 1){
+        return "rock";
+    }
+    else if (computerNumber === 2){
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
 }
 
 function roundOfRps(playerSelection, computerChoice){
+    let standing = "";
     if ((playerSelection === "rock" && computerChoice === "scissors") || (playerSelection === "paper" && computerChoice === "rock") || (playerSelection === "scissors" && computerChoice === "paper")){
         
         pscore++;
@@ -77,7 +63,9 @@ function roundOfRps(playerSelection, computerChoice){
             won();
         }
         console.log(pscore, cscore);
-        return "You win! " + playerSelection + " beats " + computerChoice;
+        standing = `You: ${pscore} - Computer: ${cscore}`;
+        standings.textContent = standing;
+        return "Your point! " + playerSelection + " beats " + computerChoice;
     }
     else if ((computerChoice === "rock" && playerSelection === "scissors") || (computerChoice === "paper" && playerSelection === "rock") || (computerChoice === "scissors" && playerSelection === "paper")){
         
@@ -86,67 +74,42 @@ function roundOfRps(playerSelection, computerChoice){
             lost();
         }
         console.log(pscore, cscore);
-        return "You lose! " + computerChoice + " beats " + playerSelection;
+        standing = `You: ${pscore} - Computer: ${cscore}`;
+        standings.textContent = standing;
+        return "Computers point! " + computerChoice + " beats " + playerSelection;
     }
     else{
         console.log(playerSelection, computerChoice);
-        return "Tie game!";
+        return "Tie!";
     }
+}
+
+function won(){
+    endresult.textContent = "Congratulations! You have won!";
+    result.style.display = "none";
+    choices.forEach(function(item){
+        item.style.pointerEvents = "none";
+    });
+}
+function lost(){
+    endresult.textContent = "You have lost!";
+    result.style.display = "none";
+    choices.forEach(function(item){
+        item.style.pointerEvents = "none";
+    });
 }
 
 reset.addEventListener("click", event => {
     pscore = 0;
     cscore = 0;
     endresult.textContent = "";
+    standings.textContent = "";
+    result.style.display = "block";
     result.textContent = "";
-    reset.style.display = "none";
-    buttons.forEach(function(item){
-        item.disabled = false;
+    choices.forEach(function(item){
+        item.style.pointerEvents = "auto";
     });
 });
-*/
 
-/*
-function game(){
-    let pwins = 0;
-    let cwins = 0;
-    let i = 0;
-    while (i < 5){
-        if (pwins === 3){
-            return "You won " + pwins + " to " + cwins;
-        }
-        else if(cwins === 3){
-            return "You lost " + pwins + " to " + cwins;
-        }
-        let result = roundOfRps(playerSelection(), getComputerChoice());
-        console.log(result);
-        if (result.substring(0,7) === "You win"){
-            pwins++;
-            i++;
-        }
-        else if (result.substring(0,8) === "You lose"){
-            cwins++;
-            i++;
-        }
-    }
-    if (pwins > cwins){
-        return "You won " + pwins + " to " + cwins;
-    }
-    else if (cwins > pwins){
-        return "You lost " + pwins + " to " + cwins;
-    }
-    else{
-        return "It was a tie";
-    }
-}
-*/
-let game = document.querySelector(".game");
-game.style.display = "none";
-let play = document.getElementById("play");
-let begin = document.querySelector(".begin");
-play.addEventListener("click", event => {
-    begin.style.display = "none";
-    game.style.display = "block";
-});
 
 
